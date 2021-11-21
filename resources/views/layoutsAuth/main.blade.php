ccebf43911e68812cd338fd857dd6665b989f018
renoalane <renopradino@gmail.com>
2021-11-21 13:49:19 +0700

    Authentikasi Model, css, main

---
+++ resources/views/layoutsAuth/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="/css/auth.css" />
+    
+    <title>{{ $title }}</title>
+</head>
+<body>
+
+    <div class="container">
+        @yield('container')
+    </div>
+
+    {{-- Bootstap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
