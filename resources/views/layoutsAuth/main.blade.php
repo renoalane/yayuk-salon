56f1cd1b2c9e7476bd3e2734c345b53728d2135d
renoalane <renopradino@gmail.com>
2021-12-13 10:33:34 +0700

    add icon

--- resources/views/layoutsAuth/main.blade.php
+++ resources/views/layoutsAuth/main.blade.php
@@ -8,7 +8,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="/css/auth.css" />
-    
+    <link rel="shortcut icon" href="{{asset('assets/img/logoYayukSalon.png')}}" type="image/x-icon" />
+
     <title>{{ $title }}</title>
 </head>
 <body>
