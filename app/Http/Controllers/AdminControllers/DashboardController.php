a98d682159a2df42908e08121f4834543d40d0ab
renoalane <renopradino@gmail.com>
2022-01-08 16:06:11 +0700

    add carbon to pick date time now

--- app/Http/Controllers/AdminControllers/DashboardController.php
+++ app/Http/Controllers/AdminControllers/DashboardController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Models\Booking;
 use App\Models\Product;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $bookings = Booking::select('code_booking', 'date', 'start_time', 'end_time', 'total_price')->orderBy('date', 'asc')->skip(0)->take(8)->get();
+        // Tanggal lebih dari sama dengan sekakrang belum
+        $bookings = Booking::select('code_booking', 'date', 'start_time', 'end_time', 'total_price')->where('date', '>=', Carbon::today())->orderBy('date', 'asc')->skip(0)->take(8)->get();
         return view('dashboard.index', [
             'active' => 'dashboard',
             'title' => 'dashboard',
