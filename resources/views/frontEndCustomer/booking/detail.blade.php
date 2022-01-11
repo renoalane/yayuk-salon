a61383ac2f043748f07a5c0562c3498ddb6fcffa
renoalane <renopradino@gmail.com>
2022-01-11 20:51:59 +0700

    fixing invoice booking

--- resources/views/frontEndCustomer/booking/detail.blade.php
+++ resources/views/frontEndCustomer/booking/detail.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css" />
-    <title>Document</title>
+    <link rel="shortcut icon" href="{{asset('assets/img/logoYayukSalon.png')}}" type="image/x-icon" />
+    <title>Invoice Booking</title>
     <style>
     .invoice-title h2, .invoice-title h3 {
         display: inline-block;
@@ -31,16 +32,19 @@
         <section class="px-lg-5 py-3">
             <div class="row">
                 <div class="col-12">
-                    <div class="invoice-title">
-                        <h2>Invoice Booking</h2>
-                        <h4 class="float-end">Code {{ $booking->code_booking }}</h4>
+                    <div class="text-center">
+                        <h2>Yayuk Salon</h2>
+                        <p class="mb-0">Jl. Raya Puhpelem, Randukuning, Puhpelem,<br> Kabupaten Wonogiri, Jawa Tengah 57698</p>
                     </div>
-                    <hr>
+                    <div class="invoice-title border-bottom">
+                        <h2>Invoice Booking</h2>
+                        <h4 class="float-end">{{ $booking->code_booking }}</h4>
+                    </div>
                     <div class="row">
                         <div class="col-6">
                             <address>
                             <strong>Nama :</strong><br>
-                                {{ $booking->user_name }}
+                                {{ $booking->name }}
                             </address>
                         </div>
                         <div class="col-6 text-end">
@@ -94,7 +98,8 @@
                                         </tr>
                                         @endforeach
                                         <tr>
-                                            <td colspan="3" class="thick-line text-end"><strong>Total</strong></td>
+                                            <td colspan="2" class="thick-line"></td>
+                                            <td class="thick-line text-center"><strong>Total</strong></td>
                                             <td class="thick-line text-end">Rp. {{ number_format($booking->total_price) }}</td>
                                         </tr>
                                     </tbody>
