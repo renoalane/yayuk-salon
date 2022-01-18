5e10d9edf6bf7e3190fd50e21f745fb52d7258ca
renoalane <renopradino@gmail.com>
2022-01-18 23:28:39 +0700

    add seeder admin adn service

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'name' => Str::random(10),
+            'username' => Str::random(5),
+            'is_admin' => 1,
+            'phone' => '8534653743',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')
+        ]);
+
+        \App\Models\Service::create([
+            'name' => Str::random(15),
+            'duration' => 20,
+            'price' => 2000,
+            'status' => 1
+        ]);
     }
 }
